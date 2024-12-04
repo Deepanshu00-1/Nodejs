bafe5d2202656a6d60358f033cdd23ab50f273bc
deepanshu <krrohan129@gmail.com>
2024-12-05 01:52:15 +0530

    day01

--- Day02/modules.js
+++ Day02/modules.js
@@ -4,3 +4,10 @@ module: file which is imported and used in code
 const math = require('./Add');
 
 console.log(math(2,5));
+
+const http = require('http');
+
+http.createServer((req, res)=>{
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.end('<h1>The sum is: math(2,5)</h1>');
+}).listen(3000);
