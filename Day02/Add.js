99d54a2f5417e2d2b4afe74461f9be5800b9d2b2
deepanshu <krrohan129@gmail.com>
2024-12-05 10:47:24 +0530

    modules

--- Day02/Add.js
+++
@@ -1,6 +0,0 @@
-const Add = (num1, num2)=>{
-    const add = num1 + num2;
-    return add;
-}
-
-module.exports = Add;
--- Day02/modules.js
+++ Day02/modules.js
@@ -1,13 +1,22 @@
 /*
 module: file which is imported and used in code
 */
-const math = require('./Add');
+const math = require('./Math');
 
-console.log(math(2,5));
+// console.log(math(2,5));
+console.log(math.addfn(2,5));
+console.log(math.subfn(2,5));
+console.log(math.mulfn(2,5));
+console.log(math.divfn(2,5));
 
 const http = require('http');
 
 http.createServer((req, res)=>{
-    res.writeHead(200,{'Content-type':'text/html'});
-    res.end('<h1>The sum is: math(2,5)</h1>');
+    if(req.url == '/math'){
+        res.writeHead(200, {'Content-type' : 'text/html'});
+        res.end(`<h1>The sum of 2 and 5 is: ${math.addfn(2,5)}</h1>`);
+    }else{
+        res.writeHead(404,{'Content-type' : 'text/html'});
+        res.end('<h1>Page not found!</h1>');
+    }
 }).listen(3000);
